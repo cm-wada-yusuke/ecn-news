e4c1d5c8d49863828c9d11c763ed19d99f863f21
wada.yusuke <wada.yusuke@classmethod.jp>
2016-12-19 09:28:02 +0900

    update: log settings and message send script.

--- script/app/SQSMessageSender.scala
+++ script/app/SQSMessageSender.scala
@@ -3,28 +3,32 @@ import java.util.UUID
 import com.amazonaws.regions.{ Region, Regions }
 import com.amazonaws.services.sqs.AmazonSQSClient
 
+import scala.concurrent.Future
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object SQSMessageSender {
 
-  val messageId = "newsWorkerAppSpec"
-  val receipt = "djfoseindvf"
+  val queueUrl: String = sys.env("NEWS_QUEUE_URL")
 
   val sqsClient = new AmazonSQSClient()
   sqsClient.setRegion(Region.getRegion(Regions.AP_NORTHEAST_1))
 
+
   def main(args: Array[String]): Unit = {
-    val queueUrl = sys.env("NEWS_QUEUE_URL")
-    // まずは100メッセージから
-    1 to 100 foreach {
-      i => send(queueUrl, i.toLong, UUID.randomUUID().toString)
-    }
-    sqsClient.shutdown()
+    //    1 to 10 foreach {
+    //      i => send((i - 1) * 1000 + 1 to i * 1000)
+    //    }
+    send(7004 to 10000)
   }
 
-  private def send(queueUrl: String, userId: Long, newsId: String): Unit = {
-    sqsClient.sendMessage(
-      queueUrl,
-      MockWorkerMessageJson.newsContentRegisterMessage(userId, newsId)
-    )
+  private def send(userIdRange: Range): Future[Unit] = Future {
+    userIdRange.foreach { i =>
+      sqsClient.sendMessage(
+        queueUrl,
+        MockWorkerMessageJson.newsContentRegisterMessage(i.toLong, UUID.randomUUID().toString)
+      )
+    }
   }
 
   object MockWorkerMessageJson {
